4c5c77bca2b7ad801016a93732c775ccb81cc5ad
a.ide <fj0088iv@aa.jp.fujitsu.com>
2015-02-09 08:46:02 +0900

    2015/2/9 a.ide

--- jmeter/jmeterAide_proxy.bat
+++
@@ -1,7 +0,0 @@
-rem '
-rem '
-rem ' jmeter with proxy
-rem ' 2014/10/21 a.ide
-rem ' 2015/2/6 a.ide プロキシのURLとポート番号、IDとパスワードは管理者へ確認。
-rem '
-call jmeterAide.bat -H PROXY_SERVER -P PROXY_PORT -u USERID -a PASSWORD
